01ba5fb797c381bd18e7d13a5cf9a081afdc76c9
ArmanSardar <mdarmansardar57@gmail.com>
2024-08-14 14:52:30 +0600

    change host

--- Library Management System/Library_Management/settings.py
+++ Library Management System/Library_Management/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-d3fi922%%+c)dyczo*65((6dxk&+$tmhx%7zqx(&kq@6@#lirr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
